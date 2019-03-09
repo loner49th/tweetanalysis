df3b69afeb136c70e51b54ecebe094f1977954f2
loner49th <loner49th@gmail.com>
2019-03-09 20:49:40 +0900

    [Add]Translator Text APIで英訳するテストコードを作成

---
+++ TranslatorTextAPITest.py
@@ -0,0 +1,38 @@
+import tweepy
+import datetime
+import pandas as pd
+
+def toEnglish(string):
+    # -*- coding: utf-8 -*-
+    import requests, uuid, json
+
+    trans_subscriptionKey = "TRANSLATOR_TEXT_KEY"
+
+    base_url = 'https://api.cognitive.microsofttranslator.com'
+    path = '/translate?api-version=3.0'
+    params = '&to=en'
+    constructed_url = base_url + path + params
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': trans_subscriptionKey,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    body = [{
+        'text' : ''
+    }]
+
+    body[0]['text'] = string
+
+    request = requests.post(constructed_url, headers=headers, json=body)
+    response = request.json()
+     
+    return response[0]['translations'][0]['text']
+
+
+if __name__ == '__main__':
+
+    keyword = "テスト"
+    toEnglish(keyword)
+
